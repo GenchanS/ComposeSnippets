3fcfbb533f969fff5a78a5f375d577233f18f2e4
GentaSasaki <gen.gen.s615@docomo.ne.jp>
2024-10-06 01:13:57 +0900

    button snippet start

--- app/src/main/java/com/example/compoasesnippets/Button.kt
+++ app/src/main/java/com/example/compoasesnippets/Button.kt
@@ -1,3 +1,25 @@
 package com.example.compoasesnippets
 
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.compoasesnippets.ui.theme.CompoaseSnippetsTheme
 
+@Composable
+fun Button1(){
+    Button(
+        onClick = {},
+        colors = ButtonDefaults.buttonColors(),
+    ) {
+        Text("Default Button")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun Button1_Preiew() {
+    Button1()
+}
