d89de9a448a1e29453259121ca7232f9e718e7ae
GentaSasaki <gen.gen.s615@docomo.ne.jp>
2024-10-06 16:49:04 +0900

    button examples

--- app/src/main/java/com/example/compoasesnippets/Button.kt
+++ app/src/main/java/com/example/compoasesnippets/Button.kt
@@ -1,25 +1,69 @@
 package com.example.compoasesnippets
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.FilledTonalButton
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.compoasesnippets.ui.theme.CompoaseSnippetsTheme
 
 @Composable
-fun Button1(){
+fun FilledButton(){
     Button(
         onClick = {},
         colors = ButtonDefaults.buttonColors(),
-    ) {
-        Text("Default Button")
+    ){
+        Text("Filled Button")
+    }
+}
+@Composable
+fun FilledTonalButton(){
+    FilledTonalButton(
+        onClick = {},
+    ){
+        Text("Tonal Button")
+    }
+}
+@Composable
+fun OutlinedButton() {
+    OutlinedButton(
+        onClick = {}
+    ){
+        Text("Outlined")
     }
 }
 
+@Composable
+fun ElevatedButton() {
+    ElevatedButton(
+        onClick = {}
+    ){
+        Text("Elevated")
+    }
+}
+
+@Composable
+fun TextButton() {
+    TextButton(
+        onClick = {}
+    ) {
+        Text("Text Button")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
-fun Button1_Preiew() {
-    Button1()
+fun ButtonPreiew() {
+    Column() {
+        FilledButton()
+        FilledTonalButton()
+        OutlinedButton()
+        ElevatedButton()
+        TextButton()
+    }
 }
