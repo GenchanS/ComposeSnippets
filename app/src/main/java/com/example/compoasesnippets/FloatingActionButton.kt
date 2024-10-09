6c6650698f85ae47aa246a6b8b649a4e6d65671f
GenchanS <s615g615@icloud.com>
2024-10-09 11:37:07 +0900

    android studioのpixelのエミュレータが動かない...

--- app/src/main/java/com/example/compoasesnippets/FloatingActionButton.kt
+++ app/src/main/java/com/example/compoasesnippets/FloatingActionButton.kt
@@ -1,9 +1,12 @@
 package com.example.compoasesnippets
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.SmallFloatingActionButton
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
@@ -15,9 +18,22 @@ fun FAB(onClick: () -> Unit) {
         Icon(Icons.Filled.Add, "Floating action button.")
     }
 }
+@Composable
+fun SmallFAB(onClick:()-> Unit){
+    SmallFloatingActionButton(
+        onClick = {onClick()},
+        containerColor = MaterialTheme.colorScheme.secondaryContainer,
+        contentColor = MaterialTheme.colorScheme.secondary
+    ) {
+        Icon(Icons.Filled.Add, "Small floating action button.")
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
 fun FButtonPreview() {
-    FAB(onClick ={} )
+    Column() {
+        FAB(onClick = {})
+        SmallFAB(onClick = {})
+    }
 }
